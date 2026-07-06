e534452c6be4c12fbaf05313c1d7de81fc066153
Noahscratch493 <161503774+Noahscratch493@users.noreply.github.com>
2026-07-06 18:24:34 +0100

    Update tw-platform.js

--- src/engine/tw-platform.js
+++ src/engine/tw-platform.js
@@ -2,6 +2,6 @@
 // This can be accessed externally on `vm.runtime.platform`
 
 module.exports = {
-    name: 'TurboWarp',
-    url: 'https://turbowarp.org/'
+    name: 'CattyMod',
+    url: 'https://cattymod.app'
 };
